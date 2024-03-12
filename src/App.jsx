ef4b9bf218decbb2e1063518e9eb48c0561227fb
havardliebe <104149882+havardliebe@users.noreply.github.com>
2024-03-12 14:44:18 +0100

    import av nye komponenter

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
-<<<<<<< HEAD
 import './styles/App.scss'
-import './components/ResourceList'
-import './assets/ressurser'
-import ResourceNav from './components/ResourceList'
-import React from 'react';
-=======
-import './App.scss'
 import './components/CategoryComponent'
 import './assets/ressurser'
 import CategoryComponent from './components/CategoryComponent'
 import ContentComponent from './components/ContentComponent'
->>>>>>> Håvard
 
 function App() {
 
   return (
-<<<<<<< HEAD
-    <>
-<body>
-=======
-      //fjernet body fra app.jsx, er allerde i index.html body cannot be child
->>>>>>> Håvard
+      
     <header>
         <nav>
             <CategoryComponent />
