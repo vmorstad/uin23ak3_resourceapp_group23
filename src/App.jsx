05c74d2a2321c7edf02472969107c48b386f185b
havardliebe <104149882+havardliebe@users.noreply.github.com>
2024-03-13 23:38:03 +0100

    endringer på style

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         <nav id="resourceNav">
             <CategoryComponent onSelectCategory={handleSelectCategory}/>
         </nav>
-        <div id="resourceContent">
+        <div id="resourceContent" class="resource-container" >
             <ContentComponent category={selectedCategory}/>
         </div>
     </header>
--- src/components/ContentComponent.jsx
+++ src/components/ContentComponent.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import resources from '../assets/ressurser'; // Importing the resources data
+import '../styles/App.scss' // la til App.scss for style til <ul>
 
 function ContentComponent({ category }) {
 
@@ -8,7 +9,7 @@ function ContentComponent({ category }) {
   return (
     <div>
       <h2>{category.toUpperCase()}</h2>
-      <ul>
+      <ul class="sources-list">
         {filteredResources.map((resource, index) => (
           <li key={index}>
             <a href={resource.url} target="_blank" rel="noopener noreferrer">{resource.title}</a>
