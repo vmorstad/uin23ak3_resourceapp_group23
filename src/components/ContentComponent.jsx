4ec3c35bc5a5d788c5d3a6fa2c66e3dc2ee99f86
havardliebe <104149882+havardliebe@users.noreply.github.com>
2024-03-21 13:00:00 +0100

    endret navn

--- src/components/ContentComponent.jsx
+++ src/components/ContentComponent.jsx
@@ -1,14 +1,14 @@
 import resources from "../assets/ressurser.js";
 
 export default function Content(props) {
-  const { activeTab } = props;
+  const { currentTab } = props;
 
   return (
       <div id="resource-container">
-        <h2>{activeTab}</h2>
+        <h2>{currentTab}</h2>
         <ul>
           {resources
-            .filter((source) => source.category === activeTab)
+            .filter((source) => source.category === currentTab)
             .map((source, index) => (
               <li key={index}>
                 <a href={source.url}>{source.title}</a>
